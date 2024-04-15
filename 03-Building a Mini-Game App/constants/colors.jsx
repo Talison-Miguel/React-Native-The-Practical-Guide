575578d5fa1e937f3e4993cd83b7b6dce087f066
Tálison Miguel <talisonmiguel84@gmail.com>
2024-04-15 13:58:16 -0300

    feat: add variables colors

---
+++ 03-Building a Mini-Game App/constants/colors.jsx
@@ -0,0 +1,10 @@
+export const Colors = {
+    primary500: "#72063c",
+    primary600: "#640233",
+    primary700: '#4e0329',
+    primaryColor800: '#3b021f',
+
+    accent500: '#ddb52f',
+
+    whiteColor: '#ffffff'
+}
