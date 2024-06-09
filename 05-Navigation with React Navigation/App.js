139624de14fc338b6406e13f6a04ed3506652a76
Talison-Miguel00 <talison.amancio@redizz.com.br>
2024-06-09 12:15:49 -0300

    add

--- 05-Navigation with React Navigation/App.js
+++ 05-Navigation with React Navigation/App.js
@@ -1,13 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { CategoriesScreen } from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { MealsOverviewScreen } from './screens/MealsOverviewScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <>
+            <StatusBar style="auto" />
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="CategoriesScreen">
+                    <Stack.Screen name='MealsCategories' component={CategoriesScreen}/>
+                    <Stack.Screen name='MealsOverviewScreen' component={MealsOverviewScreen}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
